66f2c6b481511ef85302a084043dd4654c4bfd7e
Hansen <hansen@lazynoon.com>
2021-01-12 00:25:47 +0800

    myaction-toolkit

--- src/main/java/com/lazynoon/myaction/toolkit/security/ByteMappingBaseBean.java
+++ src/main/java/com/lazynoon/myaction/toolkit/security/ByteMappingBaseBean.java
@@ -1,8 +1,20 @@
 package com.lazynoon.myaction.toolkit.security;
 
+import net_io.utils.EncodeUtils;
+
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 abstract public class ByteMappingBaseBean {
-	abstract public Map<Integer, byte[]> getByteMappingConfig();
 	abstract public boolean isProductionClass();
+	abstract public Map<Integer, String> getByteMappingString();
+
+	public Map<Integer, byte[]> getByteMappingConfig() {
+		LinkedHashMap<Integer, byte[]> result = new LinkedHashMap<Integer, byte[]>();
+		Map<Integer, String> encodedMap = getByteMappingString();
+		for (Integer key : encodedMap.keySet()) {
+			result.put(key, EncodeUtils.myBase62Decode(encodedMap.get(key)));
+		}
+		return result;
+	}
 }
